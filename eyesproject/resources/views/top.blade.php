450c97738fde7727f07df8e12a1609647756ed9a
EC2 Default User <ec2-user@ip-172-31-31-153.us-east-2.compute.internal>
2020-02-29 10:23:51 +0000

    top.blade.phpの内容新たに作成中

--- eyesproject/resources/views/top.blade.php
+++ eyesproject/resources/views/top.blade.php
@@ -5,8 +5,19 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 mx-auto">
-                                <h2>eye's project</h2>
-                                <form action="{{ action('TopController@add') }}" method="post" enctype="multipart/form-data">
+            <h2 class="text-left">eye's project</h2>
+           　<nav>
+              <ul>
+               <li><a class=”current” href=”#”>Home</a></li>
+               <li><a href=”#”>profile</a></li>
+               <li><a href=”#”>about</a></li>
+               <li><a href=”#”>details</a></li>
+               <li><a href=”#”>contact</a></li>
+               <li><a href=”#”>instagram</a></li>
+             </ul>
+             </nav>
+                                
+                                
                                     
                                     @if (count($errors) > 0)
                                     <ul>
@@ -16,29 +27,13 @@
                                     </ul>
                                     @endif
                                     
-                                    <div class="form-group row">
-                                        <label class="col-md-2" for="title">タイトル</label>
-                                        <div classs="col-md-10">
-                                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                                        </div>
-                                    </div>
                                     
-                                    <div class="form-group row">
-                                        <label class="col-md-2" for="body">本文</label>
-                                        <div class="col-md-10">
-                                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                                        </div>
-                                    </div>
                                     
-                                    <div class="form-group row">
-                                        <label class="col-md-2" for="title">画像</label>
-                                        <div class="col-md-10">
-                                            <input type="file" class="form-control-file" name="image">
-                                        </div>
-                                    </div>
+                                    
+                                    
+                                   
                                     {{ csrf_field() }}
-                                    <input type="submit" class="btn btn-primary" value="更新">
-                                </form>
+                                    
         </div>
     </div>
 </div>
